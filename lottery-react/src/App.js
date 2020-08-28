7c923e088689e96e868af54c1e2a90c3170f9151
sh383 <ssh.daniel@hanmail.net>
2020-08-28 18:57:14 -0500

    Accessing properties

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -5,14 +5,13 @@ import web3 from "./web3";
 import lottery from "./lottery";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { manager: "" };
-  }
+  state = { manager: "", players: [], balance: "" };
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
-    console.log(manager);
-    this.setState({ manager });
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
   }
 
   render() {
@@ -20,7 +19,11 @@ class App extends Component {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          This contract is managed by {this.state.manager}. There are currently{" "}
+          {this.state.players.length} people entered, competing to win{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")} ether!
+        </p>
       </div>
     );
   }

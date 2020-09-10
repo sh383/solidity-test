fe7701e84d9623ed5887800f5417009f7334bef2
sh383 <ssh.daniel@hanmail.net>
2020-09-10 13:23:28 -0500

    Single Run Compilation & error regarding "Struct containing a (nested) mapping cannot be constructed" resolved

---
+++ kickstart/ethereum/compile.js
@@ -0,0 +1,39 @@
+const path = require("path"); //cross platform compatibility
+const fs = require("fs-extra");
+const solc = require("solc");
+const { fstatSync } = require("fs-extra");
+
+const buildPath = path.resolve(__dirname, "build");
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol");
+const source = fs.readFileSync(campaignPath, "utf8");
+
+const input = {
+  language: "Solidity",
+  sources: {
+    Campaign: {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+const output = JSON.parse(solc.compile(JSON.stringify(input))).contracts
+  .Campaign;
+
+console.log(output);
+// build 폴더가 없으면 생성
+fs.ensureDirSync(buildPath);
+
+for (let contract in output) {
+  fs.outputJSONSync(
+    path.resolve(buildPath, contract + ".json"),
+    output[contract]
+  );
+}
